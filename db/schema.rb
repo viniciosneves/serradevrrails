de28acbb8bc76c82c25434bc2631434924c0b81e
Administrador <vinicios_neves@hotmail.com>
2019-08-24 11:31:01 -0400

    scaffolding Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_141158) do
+ActiveRecord::Schema.define(version: 2019_08_24_152138) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
